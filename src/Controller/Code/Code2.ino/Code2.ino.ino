a526410b3e09ab11c6601fa2d9c3aafbfbf4f8a5
Unknown <segersviktor@hotmail.com>
2018-10-10 22:19:23 +0200

    IR adding IR sending
    Random code

--- src/Controller/Code/Code2.ino/Code2.ino.ino
+++ src/Controller/Code/Code2.ino/Code2.ino.ino
@@ -1,32 +1,29 @@
-#include <IRremote.h>
-#include <IRremoteInt.h>
-IRsend irsend;
+
 volatile int sendcode = 0;
 uint8_t count = 0;
-
+int ledpin = 1;
 
 
 
 void setup()
 {
   //adc_disable();          // adc disable for low power consumption
-  DDRB = 0b00010000;
+  pinMode(ledpin, OUTPUT);
+  //DDRB = 0b00010000;
   GIMSK = 0b00100000;    // turns on pin change interrupts
-  PCMSK = 0b00000110;    // turn on interrupts on pins 6 and 7
+  PCMSK = 0b00000101;    // turn on interrupts on pins 6 and 7
   sei();
 }
 void loop() {
 
   if (sendcode == 1) {
- //   irsend.sendNEC(0xFF30CF, 32); // button1 cod   
-      PORTB = 0b00010000;
+    SendIRCodeEnable();
+    
 
   }
 
   else if (sendcode == 2) {
-    // irsend.sendNEC(0x1FE40BF, 32); // button2 code
-      PORTB = 0b00000000;
-
+    SendIRCodeShot();
   }
 
 
@@ -43,23 +40,58 @@ ISR(PCINT0_vect)
   count++;
   current = PINB; // get input state of portB as it has now changed
   changed = current ^ prev; // use XOR to find out which bit(s) have changed
-  if (changed & (1 << PB1))
+  if (changed & (1 << PB0))
   {
     //if (count >= 2) {
-      sendcode = 1;
-
-      count = 0;
+    sendcode = 1;
+  
+    count = 0;
     //}
   }
 
   if (changed & (1 << PB2)) {
     //if (count >= 2) {
 
-      // handle change on PB5
-      sendcode = 2;
-      count = 0;
-   // }
+    // handle change on PB5
+    sendcode = 2;
+    count = 0;
+    // }
   }
 
 }
 
+void IR(long microsecs) {
+
+  while (microsecs > 0) {
+    digitalWrite(ledpin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ledpin, LOW);
+    delayMicroseconds(10);
+    microsecs -= 26;
+  }
+
+}
+
+void SendIRCodeEnable() {
+  IR(500);
+  delayMicroseconds(1500);
+  IR(1500);
+  delayMicroseconds(500);
+  delay(65);
+  IR(500);
+  delayMicroseconds(1500);
+  IR(1500);
+  delayMicroseconds(500);
+}
+
+void SendIRCodeShot() {
+  IR(1500);
+  delayMicroseconds(500);
+  IR(500);
+  delayMicroseconds(1500);
+  delay(65);
+  IR(1500);
+  delayMicroseconds(500);
+  IR(500);
+  delayMicroseconds(1500);
+}
