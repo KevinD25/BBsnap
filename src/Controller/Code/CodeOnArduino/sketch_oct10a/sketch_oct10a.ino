bd198e8105b556d8355ba48be5e85baa757ff3a3
Unknown <segersviktor@hotmail.com>
2018-10-10 17:19:42 +0200

    Update arduino controller code
    Send code with the NEC protocol, the senderIR need to be on pin3

--- src/Controller/Code/CodeOnArduino/sketch_oct10a/sketch_oct10a.ino
+++ src/Controller/Code/CodeOnArduino/sketch_oct10a/sketch_oct10a.ino
@@ -1,3 +1,10 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+IRsend irsend;
+volatile int sendcode = 0;
+uint8_t count = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
   DDRB |= 0x01;
@@ -7,27 +14,49 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (sendcode == 1) {
+    irsend.sendNEC(0xFF30CF, 32); // button1 code
+    delay(200);
+  }
+
+  else if (sendcode == 2) {
+    irsend.sendNEC(0x1FE40BF, 32); // button2 code
+    delay(200);
+  }
+
+  sendcode = 0;
 
 }
 
 ISR(PCINT0_vect)
 {
-    static uint8_t prev = 0x00;
-    uint8_t current, changed;
 
-      
-    current = PINB; // get input state of portC as it has now changed
-    changed = current ^ prev; // use XOR to find out which bit(s) have changed
-    if (changed & (1 << PB1)) {
-          Serial.write("Send enable code");
-       }
-    if (changed & (1 << PB2)) {
-         // handle change on PC5
-         Serial.write("send shot code");
-
+  static uint8_t prev = 0x00;
+  uint8_t current, changed;
+  count++;
+  Serial.print(count);
+  current = PINB; // get input state of portC as it has now changed
+  changed = current ^ prev; // use XOR to find out which bit(s) have changed
+  if (changed & (1 << PB1))
+  {
+    if (count >= 2) {
+      Serial.write("Send enable code");
+      sendcode = 1;
+      count = 0;
     }
-    prev = current; // remember for next time
+  }
+
+  if (changed & (1 << PB2)) {
+    if (count >= 2) {
+
+      // handle change on PC5
+      Serial.write("send shot code");
+      sendcode = 2;
+      count = 0;
+    }
+  }
+
+
 
 
 }
