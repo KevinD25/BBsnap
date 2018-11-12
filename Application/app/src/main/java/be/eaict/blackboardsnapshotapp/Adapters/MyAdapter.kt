81b909d590994d9763823d149da95085e6adda40
Kevin Davis <kevin-davis@hotmail.com>
2018-11-12 16:59:05 +0100

    popup & image fixed

--- Application/app/src/main/java/be/eaict/blackboardsnapshotapp/Adapters/MyAdapter.kt
+++ Application/app/src/main/java/be/eaict/blackboardsnapshotapp/Adapters/MyAdapter.kt
@@ -1,11 +1,16 @@
 package be.eaict.blackboardsnapshotapp.Adapters
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import be.eaict.blackboardsnapshotapp.Objects.Picture
 import be.eaict.blackboardsnapshotapp.R
@@ -42,12 +47,20 @@ class MyAdapter(private val context: Context,
 
         val DateTextView = rowView.findViewById(R.id.txtDatum) as TextView
         val TimeTextView = rowView.findViewById(R.id.txtUur) as TextView
+        val PictureImage : ImageView = rowView.findViewById(R.id.imagePicture) as ImageView
 
         val picture = getItem(position) as Picture
 
         DateTextView.text = "TestDatum"
         TimeTextView.text = "TestUur"
+        val foto = BitmapFactory.decodeResource(context.resources,
+                R.mipmap.joren)
+        PictureImage.setImageBitmap(foto)
 
+
+        /* val type = Typeface.createFromAsset(assets, "fonts/SigmarOne.ttf")
+        txtDatum.typeface = type
+        txtUur.typeface = type*/
 
         return rowView
     }
--- Application/app/src/main/java/be/eaict/blackboardsnapshotapp/PictureActivity.kt
+++ Application/app/src/main/java/be/eaict/blackboardsnapshotapp/PictureActivity.kt
@@ -1,14 +1,19 @@
 package be.eaict.blackboardsnapshotapp
 
+import android.content.Context
 import android.content.pm.ActivityInfo
-import android.graphics.Typeface
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
 import be.eaict.blackboardsnapshotapp.Adapters.MyAdapter
-import be.eaict.blackboardsnapshotapp.Objects.PictureRepo
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_picture.*
-import kotlinx.android.synthetic.main.customlistlayout.*
 
 class PictureActivity : AppCompatActivity() {
 
@@ -28,11 +33,75 @@ class PictureActivity : AppCompatActivity() {
         val adapter = MyAdapter(this, pictureList)
         ListviewPictures.adapter = adapter
 
-        val type = Typeface.createFromAsset(assets, "fonts/SigmarOne.ttf")
-        txtDatum.typeface = type
-        txtUur.typeface = type
 
 
+
+    }
+
+    fun onClickInfo(view: View) {
+
+            // Initialize a new layout inflater instance
+            val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+            // Inflate a custom view using layout inflater
+            val view = inflater.inflate(R.layout.popup,null)
+
+            // Initialize a new instance of popup window
+            val popupWindow = PopupWindow(
+                    view, // Custom view to show in popup window
+                    LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
+                    LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+            )
+
+            // Set an elevation for the popup window
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                popupWindow.elevation = 10.0F
+            }
+
+
+            // If API level 23 or higher then execute the code
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                // Create a new slide animation for popup window enter transition
+                val slideIn = Slide()
+                slideIn.slideEdge = Gravity.TOP
+                popupWindow.enterTransition = slideIn
+
+                // Slide animation for popup window exit transition
+                val slideOut = Slide()
+                slideOut.slideEdge = Gravity.RIGHT
+                popupWindow.exitTransition = slideOut
+
+            }
+
+            // Get the widgets reference from custom view
+            val tv = view.findViewById<TextView>(R.id.text_view)
+            val buttonPopup = view.findViewById<Button>(R.id.button_popup)
+
+            // Set click listener for popup window's text view
+            tv.setOnClickListener{
+                // Change the text color of popup window's text view
+                tv.setTextColor(Color.RED)
+            }
+
+            // Set a click listener for popup's button widget
+            buttonPopup.setOnClickListener{
+                // Dismiss the popup window
+                popupWindow.dismiss()
+            }
+
+            // Set a dismiss listener for popup window
+            popupWindow.setOnDismissListener {
+                Toast.makeText(applicationContext,"Popup closed",Toast.LENGTH_SHORT).show()
+            }
+
+            // Finally, show the popup window on app
+            TransitionManager.beginDelayedTransition(root_layout)
+            popupWindow.showAtLocation(
+                    root_layout, // Location to display popup window
+                    Gravity.CENTER, // Exact position of layout to display popup
+                    0, // X offset
+                    0 // Y offset
+            )
 
     }
 }
