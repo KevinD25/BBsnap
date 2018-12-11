67b951ca074dc48dfc9d3927c27a99ed279ee3aa
Kevin Davis <kevin-davis@hotmail.com>
2018-12-11 12:33:47 +0100

    Post werkt

--- Application/app/src/main/java/be/eaict/blackboardsnapshotapp/SnapActivity.kt
+++ Application/app/src/main/java/be/eaict/blackboardsnapshotapp/SnapActivity.kt
@@ -2,33 +2,24 @@ package be.eaict.blackboardsnapshotapp
 
 import android.content.Intent
 import android.graphics.Typeface
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.MotionEvent
 import android.view.View
+import com.github.kittinunf.fuel.httpPost
 import kotlinx.android.synthetic.main.activity_snap.*
-import android.view.View.OnTouchListener
-import be.eaict.blackboardsnapshotapp.Objects.DataFile
-import com.beust.klaxon.Json
-import com.beust.klaxon.JsonObject
-import com.beust.klaxon.Parser
-import com.beust.klaxon.json
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.result.Result
-import com.google.gson.GsonBuilder
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
-import java.net.URL
 
 
 class SnapActivity : AppCompatActivity() {
 
     private lateinit var result: String
     private var userID: Int = 0
+    private var photoTaken: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +36,7 @@ class SnapActivity : AppCompatActivity() {
 
     fun sendSnapCommand(view: View){
         userID = 1
+        photoTaken = false
 
 
         val requestParams = ArrayList<Pair<String, String>>(1)
@@ -53,20 +45,29 @@ class SnapActivity : AppCompatActivity() {
 
         val json = JSONObject()
         json.put("studnr", userID)
+        val jsonbody = json.toString()
 
 
         doAsync{
-            val (ignoredRequest, ignoredResponse, result) =
-                    Fuel.post("http://brabo2.ddns.net:555/takephoto/")
-                            .body(json.toString())
-                            .responseString()
 
 
-            result.fold(success = {
-                println(it.toString())
-            }, failure = {
-                println(String(it.errorData))
-            })
+
+            val request = "http://brabo2.ddns.net:555/takephoto/".httpPost().body(jsonbody)
+            request.headers["Content-Type"] = "application/json"
+            request.response { request, response, result ->
+                Log.e("Result", response.toString())
+                if(response.responseMessage == "OK"){
+                    photoTaken = true
+                    longToast("Photo taken!")
+
+                }
+                if (photoTaken){
+                    btnSnap.isEnabled = false
+                    Handler().postDelayed({   //TODO Timer werkt niet
+                        btnSnap.isEnabled = true
+                    }, 3000)
+                }
+            }
 
             uiThread{
 
