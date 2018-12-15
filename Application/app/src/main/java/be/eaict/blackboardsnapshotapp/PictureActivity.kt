1c3f6843a3bd3d9d7ae89130d4840888fc3b7f26
Kevin Davis <kevin-davis@hotmail.com>
2018-12-15 02:27:13 +0100

    Download works

--- Application/app/src/main/java/be/eaict/blackboardsnapshotapp/PictureActivity.kt
+++ Application/app/src/main/java/be/eaict/blackboardsnapshotapp/PictureActivity.kt
@@ -1,37 +1,72 @@
 package be.eaict.blackboardsnapshotapp
 
-import android.app.ProgressDialog
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.ActivityInfo
-import android.os.AsyncTask
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.*
+import android.support.annotation.UiThread
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.transition.Slide
 import android.transition.TransitionManager
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.webkit.DownloadListener
+import android.webkit.MimeTypeMap
+import android.webkit.URLUtil
 import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.PopupWindow
 import android.widget.Toast
 import be.eaict.blackboardsnapshotapp.Adapters.MyAdapter
-import be.eaict.blackboardsnapshotapp.Objects.DataFile
-import be.eaict.blackboardsnapshotapp.Objects.Foto
-import be.eaict.blackboardsnapshotapp.Objects.Repository
+import be.eaict.blackboardsnapshotapp.Objects.*
+import com.bumptech.glide.Glide
+import com.github.kittinunf.fuel.Fuel
+import com.squareup.picasso.Picasso
+import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_picture.*
-import kotlinx.android.synthetic.main.customlistlayout.*
-import java.io.BufferedInputStream
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.Okio
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.*
+import java.lang.Exception
 import java.net.URL
-import java.net.URLConnection
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class PictureActivity : AppCompatActivity() {
 
     lateinit var fotos: ArrayList<Foto>
     lateinit var data: DataFile
+    lateinit var context: Context
+    lateinit var activity: Activity
+    private lateinit var downloadPage: String
+    lateinit var downloadListener: DownloadListener
+    var writeAccess = false
+
+    /** Permission Request Code */
+    private val PERMISSION_REQUEST_CODE = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,13 +74,17 @@ class PictureActivity : AppCompatActivity() {
         this.setRequestedOrientation(
                 ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
+        /** Application Context and Main Activity */
+        context = applicationContext
+        activity = this
+
         data = Repository.getInstance().photos
         fotos = data.fotos
 
         val adapter = MyAdapter(this, fotos)
         ListviewPictures.adapter = adapter
-        btnDownload.setOnClickListener(myButtonClickListener)
 
+        checkWriteAccess()
     }
 
     fun createPopUp(view: View) {
@@ -122,23 +161,113 @@ class PictureActivity : AppCompatActivity() {
 
     }
 
+    fun onClickDownload(v: View) {
+        val parentRow = v.parent as View
+        val listView = parentRow.parent as ListView
+        val position = listView.getPositionForView(parentRow)
+        val cameraID: Int
+        val photoName: String
 
-    private val myButtonClickListener = object : View.OnClickListener {
-        override fun onClick(v: View) {
-            val parentRow = v.parent as View
-            val listView = parentRow.parent as ListView
-            val position = listView.getPositionForView(parentRow)
-            val cameraID: Int
-            val photoName: String
+        cameraID = fotos[position].camera.id
+        photoName = fotos[position].naam
 
-            cameraID = fotos[position].camera.id
-            photoName = fotos[position].naam
+        downloadPage = "http://brabo2.ddns.net:555/photo/downloadphoto/" + cameraID + "/" + photoName
+        downloadPage = "http://brabo2.ddns.net:555/photo/getphoto/" + cameraID + "/" + photoName
 
-            URL("http://brabo2.ddns.net:555/photo/downloadphoto/" + cameraID + "/" + photoName).file
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        100)
+
+            } else {
+                saveImageToStorage(cameraID, photoName)
+                Toast.makeText(this, "Image saved!", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            saveImageToStorage(cameraID, photoName)
+            Toast.makeText(this, "Image saved!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun saveImageToStorage(cameraID: Int, photoName: String) {
+
+
+        val externalStorageState = Environment.getExternalStorageState()
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
+            val storageDirectory = Environment.getExternalStorageDirectory().toString()
+            val directory = File(storageDirectory + "/DCIM/BBsnap")
+            directory.mkdirs()
+            val file = File(directory, photoName)
+                val stream: OutputStream = FileOutputStream(file)
+                //var drawable = Glide.with(context).load("http://brabo2.ddns.net:555/photo/getphoto/" + cameraID + "/" + photoName).submit()
+                // var bitmap = (drawable as BitmapDrawable).bitmap
+                val me = this
+                doAsync {
+                    val input: InputStream = URL(downloadPage).openStream()
+                    var bitmap: Bitmap = BitmapFactory.decodeStream(input)
+
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                    stream.flush()
+                    stream.close()
+
+                }
+
+        } else {
+            Toast.makeText(this, "Unable to access the storage", Toast.LENGTH_SHORT).show()
+        }
+    }
+    
+    private fun checkWriteAccess() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /**
+             * Check for permission status.
+             * */
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    val builder = AlertDialog.Builder(activity)
+                    builder.setMessage("Required permission to write external storage to save downloaded file.")
+                    builder.setTitle("Please Grant Write Permission")
+                    builder.setPositiveButton("OK") { _, _ ->
+                        ActivityCompat.requestPermissions(
+                                activity,
+                                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                PERMISSION_REQUEST_CODE
+                        )
+                    }
+                    builder.setNeutralButton("Cancel", null)
+                    val dialog = builder.create()
+                    dialog.show()
+                } else {
+                    ActivityCompat.requestPermissions(
+                            activity,
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            PERMISSION_REQUEST_CODE
+                    )
+                }
+            } else {
+                /**
+                 * Already have required permission.
+                 * */
+                writeAccess = false
+            }
         }
     }
 
-    fun downloadPhoto(){
-
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    writeAccess = true
+                } else {
+                    // Permission denied
+                    writeAccess = false
+                    Toast.makeText(context, "Permission Denied. This app will not work with right permission.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
+
