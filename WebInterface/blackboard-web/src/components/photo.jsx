ca712b6f2b3d0f9dfcc5a5db4cfb2c7da9f651da
Joren Jamar <jorenjamar@hotmail.com>
2018-12-16 16:43:24 +0100

    enable disable knop toegevoegd aan website

--- WebInterface/blackboard-web/src/components/photo.jsx
+++ WebInterface/blackboard-web/src/components/photo.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 
 class Photo extends Component {
-  state = { foto: ".../images/joren.jpg", naam: "lelijk heufd" };
+  state = {
+    fotolink:
+      "http://brabo2.ddns.net:555/photo/getphoto/10/" + this.props.photo,
+    fotonaam: this.props.photo
+  };
+
   render() {
     return (
       <div>
         <img
           class="mr-3"
-          src="https://i1.sndcdn.com/avatars-000050933768-gqr14u-t500x500.jpg"
+          src={this.state.fotolink}
           alt="lelijk heufd"
           height="240px"
-          width="240px"
         />
-        <p>Joren in het dagelijkse leven</p>
+        <p>{this.state.fotonaam}</p>
       </div>
     );
   }
