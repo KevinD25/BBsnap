d1cbf7f56c937a0823d8a5ee052fe4fd81952d7a
joeri <joeri.temmerman@student.ap.be>
2018-12-20 13:52:20 +0000

    fixed mqtt

--- src/BBSnap/BBS_Config.py
+++ src/BBSnap/BBS_Config.py
@@ -3,5 +3,5 @@ LED_PIN = 17                # pin of the enable/disable led, also stores enable/
 INPUT = 18                  # pin of IR receiver
 URL = "http://brabo2.ddns.net:555/photo" #URL of photo post endpoint
 UNIT_ID = 10                # device ID
-MQTT_BROKER = "localhost"   # addres of mqtt broker
+MQTT_BROKER = "brabo2.ddns.net"   # addres of mqtt broker
 BROKER_PORT = 1883          # port of mqtt at broker
